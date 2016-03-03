1dd320fcbf93fc2a0f3645a277716e00e0c78b29
dinever <dinever@live.cn>
2016-03-03 02:10:35 -0500

    [style] Improved style.

--- cmd/pararoxy/main.go
+++ cmd/pararoxy/main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-  "net"
-  "log"
-  "github.com/dinever/pararoxy"
+	"github.com/dinever/pararoxy"
+	"log"
+	"net"
 )
 
 func main() {
-  listener, err := net.Listen("tcp", ":3316")
-  if err != nil {
-    log.Fatal(err)
-  }
+	listener, err := net.Listen("tcp", ":3316")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  for {
-    conn, err := listener.Accept()
-    if err != nil {
-      log.Println(err)
-      continue
-    }
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
 
-    go Pararoxy.Proxy(conn)
-  }
+		go Pararoxy.Proxy(conn)
+	}
 }
